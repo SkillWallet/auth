dab1c6fe1555f3896ba1d5d452509707aa06d49f
mchrupcala <michael.chrupcala@gmail.com>
2021-07-26 22:18:26 -0400

    setting default key in tsx

--- src/components/skillwallet-auth/skillwallet-auth.tsx
+++ src/components/skillwallet-auth/skillwallet-auth.tsx
@@ -9,7 +9,7 @@ import * as buffer from 'buffer';
   shadow: true,
 })
 export class SkillwalletAuth {
-  @Prop({mutable: true}) key: string;
+  @Prop({mutable: true}) key: string = 'd0aa09caba3ee6e60eb4b2724e9909df5328c599';
   @State() community: any;
   @Event({
     eventName: 'showLogin',
