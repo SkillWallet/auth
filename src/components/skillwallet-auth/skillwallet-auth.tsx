b76a47a38ad532b67b484a08c42b32540f25175d
mchrupcala <michael.chrupcala@gmail.com>
2021-07-26 22:58:42 -0400

    last try

--- src/components/skillwallet-auth/skillwallet-auth.tsx
+++ src/components/skillwallet-auth/skillwallet-auth.tsx
@@ -9,7 +9,7 @@ import * as buffer from 'buffer';
   shadow: true,
 })
 export class SkillwalletAuth {
-  @Prop({mutable: true}) partnerKey: string = "d0aa09caba3ee6e60eb4b2724e9909df5328c599";
+  @Prop() partnerKey: string = "d0aa09caba3ee6e60eb4b2724e9909df5328c599";
   @State() community: any;
   @Event({
     eventName: 'showLogin',
