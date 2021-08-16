ea94c79053eaf85fffbf636f9f783d2f437ce029
mchrupcala <michael.chrupcala@gmail.com>
2021-08-16 13:21:03 -0400

    User can click background screen to close modal window. Refactored app for better control flow btwn components

--- src/components/skillwallet-auth/skillwallet-auth.tsx
+++ src/components/skillwallet-auth/skillwallet-auth.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, EventEmitter, Prop, h, State } from '@stencil/core';
+import { Component, Event, EventEmitter, Prop, h, State, Listen } from '@stencil/core';
 import { getCommunity } from '../../utils/utils';
 import * as buffer from 'buffer';
 
@@ -10,18 +10,18 @@ import * as buffer from 'buffer';
 export class SkillwalletAuth {
   @Prop() partnerKey: string;
   @State() community: any;
+  @State() displayLogin: boolean;
   @State() userUploadedImage: any = '';
   @State() icon: any = this.userUploadedImage === '' ? 
         <auth-image class="person-img" image={"https://skillwallet-demo-images.s3.us-east-2.amazonaws.com/user.svg"}></auth-image> :
         <auth-image class="person-img" image={this.userUploadedImage}></auth-image>
-  @Event({
-    eventName: 'showLogin',
-    composed: true,
-    cancelable: true,
-    bubbles: true,
-  })
-  showLogin: EventEmitter<Boolean>;
-  @State() clickCount: number = 0;
+  
+  @State() usersIsVisible: boolean = false;
+  @State() qrIsVisible: boolean = false;
+  @State() newUserIsVisible: boolean = false;
+  @State() userDetailsAreVisible: boolean = false;
+  @State() userRoleIsVisible: boolean = false;
+  @State() qrText: string = null;
 
   async componentDidLoad() {
     (window as any).Buffer = buffer;
@@ -29,21 +29,91 @@ export class SkillwalletAuth {
     this.community = comm;
   }
 
+  @Event({
+    eventName: 'showLogin',
+    composed: true,
+    cancelable: true,
+    bubbles: true,
+  })
+  showLogin: EventEmitter<Boolean>;
+
+  handleHideClick() {
+    this.displayLogin = false;
+    
+    this.usersIsVisible = false;
+    this.qrIsVisible = false;
+    this.newUserIsVisible = false;
+    this.userDetailsAreVisible = false;
+    this.userRoleIsVisible = false;
+  }
+
+  handleClickPropagation(e) {
+    e.stopPropagation();
+  }
+
+  @Listen('showUserDetails')
+  showUserDetails() {
+    this.newUserIsVisible = false;
+    this.userDetailsAreVisible = true;
+  }
+
+  @Listen('showUserRole')
+  showUserRole() {
+    this.userDetailsAreVisible = false;
+    this.userRoleIsVisible = true;
+  }
+
+  handleQRClick = () => {
+    this.usersIsVisible = false;
+    this.qrText = 'skillwallet';
+    this.qrIsVisible = true;
+  };
+
   handleClick() {
-    this.clickCount += 1;
-    if (this.clickCount < 2) {
-      this.showLogin.emit(true);
+    this.displayLogin = true;
+    this.usersIsVisible = true;
+  }
+
+  @Listen('showNewScreen')
+  handleNewScreen(text) {
+    this.usersIsVisible = false;
+    this.userRoleIsVisible = false;
+
+    if (text.detail) {
+      this.qrText = text.detail;
+      this.qrIsVisible = true;
+    } else {
+      this.newUserIsVisible = true;
     }
   }
 
   render() {
     return (
-      <div onClick={() => this.handleClick()}>
-        <button class="connect-wallet-button">
-          {this.icon}
-          <p>Connect Wallet</p>
-        </button>
-        <users-modal community={this.community}  userUploadedImage={this.userUploadedImage}></users-modal>
+        <div>
+          <button class="connect-wallet-button" onClick={() => this.handleClick()}>
+            {this.icon}
+            <p>Connect Wallet</p>
+          </button>
+
+          {this.displayLogin ?
+              <div class="background-screen" onClick={() => this.handleHideClick()}>
+                <div class="topDiv">
+                  <div class="modalWindow" onClick={(event) => this.handleClickPropagation(event)}>
+              
+              {(this.usersIsVisible === true) ? <users-modal userUploadedImage={this.userUploadedImage}></users-modal> : null}
+
+              {this.qrIsVisible === true ? <qr-modal community={this.community} textKey={this.qrText}></qr-modal> : null}
+              {this.newUserIsVisible      === true ? <new-user community={this.community}></new-user> : null}
+              {this.userDetailsAreVisible === true ? 
+                  <user-details 
+                    community={this.community} 
+                    validator={{user: {name: 'length', options: {min: 4, max: 17}}, file: {name: 'file', options: []}}}
+                  ></user-details> : null}
+              {this.userRoleIsVisible     === true ? <user-role community={this.community}></user-role> : null}
+              </div>
+
+          </div>
+        </div> : null}
       </div>
     );
   }
